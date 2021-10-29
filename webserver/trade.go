403cb8ebf544c139b80324d9c7d6e5a901deece9
romanornr <romanornr@gmail.com>
2021-10-30 01:20:52 +0200

    Placing order success market

--- webserver/trade.go
+++ webserver/trade.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // TradeHandler handleHome is the handler for the '/trade' page request.
@@ -39,10 +40,10 @@ func getTradeResponse(w http.ResponseWriter, r *http.Request) {
 func TradeCtx(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
 		exchangeNameReq := chi.URLParam(request, "exchange")
-		base:= currency.NewCode(chi.URLParam(request, "base"))
+		base := currency.NewCode(chi.URLParam(request, "base"))
 		quote := currency.NewCode(chi.URLParam(request, "quote"))
 		qtyUSD, err := strconv.ParseFloat(chi.URLParam(request, "qty"), 32)
-		if err != nil{
+		if err != nil {
 			logrus.Errorf("failed to parse qty %s\n", err)
 		}
 
@@ -58,18 +59,21 @@ func TradeCtx(next http.Handler) http.Handler {
 			assetItem = asset.Spot
 		}
 
-		//var orderType order.Type
-		//var side order.Side
-		//var postOnly bool
-		//switch strings.ToLower(chi.URLParam(request, "orderType")) {
-		//case "marketBuy":
-		//	orderType = order.Market
-		//	side = order.Buy
-		//case "limitBuy":
-		//	orderType = order.Limit
-		//	side = order.Sell
-		//	postOnly = true
-		//}
+		var orderType order.Type
+		var side order.Side
+		var postOnly bool
+
+		switch chi.URLParam(request, "orderType") {
+		case "marketBuy":
+			orderType = order.Market
+			side = order.Bid
+		case "limitBuy":
+			orderType = order.Limit
+			side = order.Ask
+			postOnly = true
+		}
+
+		logrus.Printf("order side %s\n", side)
 
 		d := GetDealerInstance()
 		e, err := d.ExchangeManager.GetExchangeByName(exchangeNameReq)
@@ -78,7 +82,7 @@ func TradeCtx(next http.Handler) http.Handler {
 			return
 		}
 
-		// try to find out how to enable all pairs?? 
+		// try to find out how to enable all pairs??
 		d.Settings.EnableAllPairs = true
 		d.Settings.EnableCurrencyStateManager = true
 
@@ -88,7 +92,7 @@ func TradeCtx(next http.Handler) http.Handler {
 		}
 
 		logrus.Printf("all parts enabled: %v\n", x)
-		
+
 		if err = e.UpdateCurrencyStates(context.Background(), asset.Spot); err != nil {
 			logrus.Errorf("currency state update failed: %s\n", err)
 		}
@@ -100,63 +104,68 @@ func TradeCtx(next http.Handler) http.Handler {
 
 		logrus.Info(f)
 
-		if err = e.CanTrade(base, assetItem); err != nil{
+		if err = e.CanTrade(base, assetItem); err != nil {
 			logrus.Errorf("Can not trade: %s\n", err)
 		} // currency state fails
-
 
 		if err = e.CanTradePair(p, assetItem); err != nil {
 			logrus.Errorf("can not trade pair %s\n", err)
 		}
 
 		price, err := e.UpdateTicker(context.Background(), p, assetItem)
+		if err != nil {
+			logrus.Errorf("failed to update ticker %s\n", err)
+		}
 
 		qty := qtyUSD / price.Ask
-
 		logrus.Printf("quantity %f\n", qty)
 
-		//d.GetActiveOrders(context.Background(), e.GetName(), )
+		o := order.Submit{
+			ImmediateOrCancel: false,
+			HiddenOrder:       false,
+			FillOrKill:        false,
+			PostOnly:          postOnly,
+			ReduceOnly:        false,
+			Leverage:          0,
+			Price:             price.Ask,
+			Amount:            qty,
+			StopPrice:         0,
+			LimitPriceUpper:   0,
+			LimitPriceLower:   0,
+			TriggerPrice:      0,
+			TargetAmount:      0,
+			ExecutedAmount:    0,
+			RemainingAmount:   0,
+			Fee:               0,
+			Exchange:          e.GetName(),
+			InternalOrderID:   "",
+			ID:                "",
+			AccountID:         "",
+			ClientID:          "",
+			ClientOrderID:     "",
+			WalletAddress:     "",
+			Offset:            "",
+			Type:              orderType,
+			Side:              side,
+			Status:            "",
+			AssetType:         assetItem,
+			Date:              time.Now(),
+			LastUpdated:       time.Time{},
+			Pair:              p,
+			Trades:            nil,
+		}
 
-		//e.SetPairs()
-		//currency.NewPair()
-		//
-		//
-		//order.Submit{
-		//	ImmediateOrCancel: false,
-		//	HiddenOrder:       false,
-		//	FillOrKill:        false,
-		//	PostOnly:          postOnly,
-		//	ReduceOnly:        false,
-		//	Leverage:          0,
-		//	Price:             0,
-		//	Amount:            0,
-		//	StopPrice:         0,
-		//	LimitPriceUpper:   0,
-		//	LimitPriceLower:   0,
-		//	TriggerPrice:      0,
-		//	TargetAmount:      0,
-		//	ExecutedAmount:    0,
-		//	RemainingAmount:   0,
-		//	Fee:               0,
-		//	Exchange:          "",
-		//	InternalOrderID:   "",
-		//	ID:                "",
-		//	AccountID:         "",
-		//	ClientID:          "",
-		//	ClientOrderID:     "",
-		//	WalletAddress:     "",
-		//	Offset:            "",
-		//	Type:              "",
-		//	Side:              "",
-		//	Status:            "",
-		//	AssetType:         "",
-		//	Date:              time.Time{},
-		//	LastUpdated:       time.Time{},
-		//	Pair:              currency.Pair{},
-		//	Trades:            nil,
-		//}
+		if err = o.Validate(); err != nil {
+			logrus.Errorf("failed to validate order: %s\n", err)
+		}
+
+		orderResponse, err := e.SubmitOrder(context.Background(), &o)
+		if err != nil {
+			logrus.Errorf("submit order failed: %s\n", err)
+		}
+		logrus.Printf("order response ID %s placed %t", orderResponse.OrderID, orderResponse.IsOrderPlaced)
+
 		ctx := context.WithValue(request.Context(), "response", order.Submit{})
 		next.ServeHTTP(w, request.WithContext(ctx))
-
 	})
 }
