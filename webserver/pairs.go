55d326e66e805ea9767d74f04a832d9fc85c3434
romanornr <romanornr@gmail.com>
2021-11-03 03:42:43 +0100

    gofmt

--- webserver/pairs.go
+++ webserver/pairs.go
@@ -42,22 +42,20 @@ func FetchPairsCtx(next http.Handler) http.Handler {
 		//	}
 		//	for _, p := range pairs {
 
-
 		for _, a := range assetTypes {
 
-				c, err := e.GetAvailablePairs(a)
-				if err != nil {
-					logrus.Errorf("Failed to get enabled pairs: %s\n", err)
-				}
-
-				formattedPair := c.Format("-", "", true)
-				for _, p := range formattedPair {
-					response.Pair = append(response.Pair, pair{Name: p.String(), AssetType: a})
-				}
-				//response.Pair = append(response.Pair, pair{Name: formattedPair, AssetType: a})
-				//response.Pair = append(response.Pair, pair{Name: p, AssetType: a})
+			c, err := e.GetAvailablePairs(a)
+			if err != nil {
+				logrus.Errorf("Failed to get enabled pairs: %s\n", err)
 			}
 
+			formattedPair := c.Format("-", "", true)
+			for _, p := range formattedPair {
+				response.Pair = append(response.Pair, pair{Name: p.String(), AssetType: a})
+			}
+			//response.Pair = append(response.Pair, pair{Name: formattedPair, AssetType: a})
+			//response.Pair = append(response.Pair, pair{Name: p, AssetType: a})
+		}
 
 		request = request.WithContext(context.WithValue(request.Context(), "response", response))
 		next.ServeHTTP(w, request)
