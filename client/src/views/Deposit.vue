0a0e05d3cdeb48239aa6782297d27f0e194e0466
romanornr <romanornr@gmail.com>
2023-04-15 20:25:23 +0200

    Vue Deposit set Binance as default exchange

--- client/src/views/Deposit.vue
+++ client/src/views/Deposit.vue
@@ -3,119 +3,120 @@
     <div class="input-group mb-3">
       <!-- {{template "exchangesRadioButtons"}} -->
       <span class="input-group-text" id="basic-addon">Exchange</span>
-      <input
-        type="radio"
-        class="btn-check"
-        name="options-outlined"
-        v-model="exchangeName"
-        value="ftx"
-        id="ftx-btn-check-outlined"
-        autocomplete="off"
-        checked
-        required />
-      <label class="btn btn-outline-primary" for="ftx-btn-check-outlined"
-        >FTX</label
-      ><br />
 
       <input
-        type="radio"
-        class="btn-check"
-        name="options-outlined"
-        v-model="exchangeName"
-        value="binance"
-        id="binance-btn-check-2-outlined"
-        autocomplete="off" />
+          type="radio"
+          class="btn-check"
+          name="options-outlined"
+          v-model="exchangeName"
+          value="binance"
+          id="binance-btn-check-2-outlined"
+          autocomplete="off"/>
       <label class="btn btn-outline-warning" for="binance-btn-check-2-outlined"
-        >Binance</label
-      ><br />
+      >Binance</label>
+      <br/>
+
+<!--      <input-->
+<!--          type="radio"-->
+<!--          class="btn-check"-->
+<!--          name="options-outlined"-->
+<!--          v-model="exchangeName"-->
+<!--          value="ftx"-->
+<!--          id="ftx-btn-check-outlined"-->
+<!--          autocomplete="off"-->
+<!--          checked-->
+<!--          required/>-->
+<!--      <label class="btn btn-outline-primary" for="ftx-btn-check-outlined"-->
+<!--      >FTX</label-->
+<!--      ><br/>-->
 
       <input
-        type="radio"
-        class="btn-check"
-        name="options-outlined"
-        v-model="exchangeName"
-        value="deribit"
-        id="deribit-success-outlined"
-        autocomplete="off" />
+          type="radio"
+          class="btn-check"
+          name="options-outlined"
+          v-model="exchangeName"
+          value="deribit"
+          id="deribit-success-outlined"
+          autocomplete="off"/>
       <label class="btn btn-outline-success" for="deribit-success-outlined"
-        >Deribit</label
+      >Deribit</label
       >
 
       <input
-        type="radio"
-        class="btn-check"
-        name="options-outlined"
-        v-model="exchangeName"
-        value="bitmex"
-        id="bitmex-danger-outlined"
-        autocomplete="off" />
+          type="radio"
+          class="btn-check"
+          name="options-outlined"
+          v-model="exchangeName"
+          value="bitmex"
+          id="bitmex-danger-outlined"
+          autocomplete="off"/>
       <label class="btn btn-outline-danger" for="bitmex-danger-outlined"
-        >Bitmex</label
+      >Bitmex</label
       >
 
       <input
-        type="radio"
-        class="btn-check"
-        name="options-outlined"
-        v-model="exchangeName"
-        value="huobi"
-        id="huobi-info-outlined"
-        autocomplete="off" />
+          type="radio"
+          class="btn-check"
+          name="options-outlined"
+          v-model="exchangeName"
+          value="huobi"
+          id="huobi-info-outlined"
+          autocomplete="off"/>
       <label class="btn btn-outline-info" for="huobi-info-outlined"
-        >Huobi</label
+      >Huobi</label
       >
 
       <input
-        type="radio"
-        class="btn-check"
-        name="options-outlined"
-        v-model="exchangeName"
-        value="bitfinex"
-        id="finex-success-outlined"
-        autocomplete="off" />
+          type="radio"
+          class="btn-check"
+          name="options-outlined"
+          v-model="exchangeName"
+          value="bitfinex"
+          id="finex-success-outlined"
+          autocomplete="off"/>
       <label class="btn btn-outline-success" for="finex-success-outlined"
-        >Bitfinex</label
+      >Bitfinex</label
       >
 
       <input
-        type="radio"
-        class="btn-check"
-        name="options-outlined"
-        v-model="exchangeName"
-        value="btse"
-        id="btse-info-outlined"
-        autocomplete="off" />
+          type="radio"
+          class="btn-check"
+          name="options-outlined"
+          v-model="exchangeName"
+          value="btse"
+          id="btse-info-outlined"
+          autocomplete="off"/>
       <label class="btn btn-outline-info" for="btse-info-outlined">BTSE</label>
 
       <input
-        type="radio"
-        class="btn-check"
-        name="options-outlined"
-        v-model="exchangeName"
-        value="kraken"
-        id="kraken-dark-outlined"
-        autocomplete="off" />
+          type="radio"
+          class="btn-check"
+          name="options-outlined"
+          v-model="exchangeName"
+          value="kraken"
+          id="kraken-dark-outlined"
+          autocomplete="off"/>
       <label class="btn btn-outline-dark" for="kraken-dark-outlined"
-        >Kraken</label
+      >Kraken</label
       >
 
       <input
-        type="radio"
-        class="btn-check"
-        name="options-outlined"
-        v-model="exchangeName"
-        value="bittrex"
-        id="bittrex-info-outlined"
-        autocomplete="off" />
+          type="radio"
+          class="btn-check"
+          name="options-outlined"
+          v-model="exchangeName"
+          value="bittrex"
+          id="bittrex-info-outlined"
+          autocomplete="off"/>
     </div>
 
     <div class="row">
       <div class="col">
         <v-select
-          v-model="asset"
-          :options="assets"
-          label="code"
-          placeholder="BTC"></v-select>
+            v-model="asset"
+            :options="assets"
+            label="code"
+            placeholder="BTC"></v-select>
       </div>
     </div>
 
@@ -123,69 +124,69 @@
     <div class="input-group mb-3">
       <div class="form-check form-check-inline">
         <input
-          class="form-check-input"
-          type="radio"
-          name="inlineRadioOptions"
-          v-model="chain"
-          id="inlineRadio1"
-          value="default"
-          checked />
+            class="form-check-input"
+            type="radio"
+            name="inlineRadioOptions"
+            v-model="chain"
+            id="inlineRadio1"
+            value="default"
+            checked/>
         <label class="form-check-label" for="inlineRadio1">Default</label>
       </div>
       <div class="form-check form-check-inline">
         <input
-          class="form-check-input"
-          type="radio"
-          name="inlineRadioOptions"
-          v-model="chain"
-          id="inlineRadio2"
-          value="erc20" />
+            class="form-check-input"
+            type="radio"
+            name="inlineRadioOptions"
+            v-model="chain"
+            id="inlineRadio2"
+            value="erc20"/>
         <label class="form-check-label" for="inlineRadio2">ERC20</label>
       </div>
       <div class="form-check form-check-inline">
         <input
-          class="form-check-input"
-          type="radio"
-          name="inlineRadioOptions"
-          v-model="chain"
-          id="inlineRadio3"
-          value="trx" />
+            class="form-check-input"
+            type="radio"
+            name="inlineRadioOptions"
+            v-model="chain"
+            id="inlineRadio3"
+            value="trx"/>
         <label class="form-check-label" for="inlineRadio2">TRX</label>
       </div>
 
       <div class="form-check form-check-inline">
         <input
-          class="form-check-input"
-          type="radio"
-          name="inlineRadioOptions"
-          v-model="chain"
-          id="inlineRadio4"
-          value="sol" />
+            class="form-check-input"
+            type="radio"
+            name="inlineRadioOptions"
+            v-model="chain"
+            id="inlineRadio4"
+            value="sol"/>
         <label class="form-check-label" for="inlineRadio3">SOL</label>
       </div>
 
       <div class="form-check form-check-inline">
         <input
-          class="form-check-input"
-          type="radio"
-          name="inlineRadioOptions"
-          v-model="chain"
-          id="inlineRadio5"
-          value="BNB" />
+            class="form-check-input"
+            type="radio"
+            name="inlineRadioOptions"
+            v-model="chain"
+            id="inlineRadio5"
+            value="BNB"/>
         <label class="form-check-label" for="inlineRadio3">BNB</label>
       </div>
     </div>
 
     <button class="btn btn-primary" type="submit" :disabled="loading">
       <span
-        v-if="loading"
-        class="spinner-border spinner-border-sm"
-        role="status"
-        aria-hidden="true"></span>
+          v-if="loading"
+          class="spinner-border spinner-border-sm"
+          role="status"
+          aria-hidden="true"></span>
       <span v-if="loading">Loading...</span><span v-else>Deposit</span>
     </button>
   </form>
-  <br />
+  <br/>
 
   <div v-if="loading" class="spinner-border text-primary" role="status">
     <span class="sr-only"></span>
@@ -223,16 +224,16 @@ export default {
       assetData: '',
       error: '',
       options: [
-        { text: 'FTX', value: 'ftx' },
-        { text: 'Binance', value: 'binance' },
-        { text: 'Deribit', value: 'deribit' },
-        { text: 'Kraken', value: 'kraken' },
-        { text: 'BTSE', value: 'btse' },
+        {text: 'Binance', value: 'binance'},
+        {text: 'FTX', value: 'ftx'},
+        {text: 'Deribit', value: 'deribit'},
+        {text: 'Kraken', value: 'kraken'},
+        {text: 'BTSE', value: 'btse'},
       ],
-      asset: { code: 'USDT' },
+      asset: {code: 'USDT'},
       assets: [],
       exchangeInput: '',
-      exchangeName: 'ftx',
+      exchangeName: 'binance',
       address: '',
       balance: '',
       balanceUSD: '',
@@ -249,7 +250,7 @@ export default {
 
       try {
         const {
-          data: { assets },
+          data: {assets},
         } = await this.$api.get(`assets/${this.exchangeName}`)
 
         this.assets = assets
@@ -270,7 +271,7 @@ export default {
 
       try {
         const response = await this.$api.get(
-          `deposit/${this.exchangeName}/${this.asset.code}/${this.chain}`
+            `deposit/${this.exchangeName}/${this.asset.code}/${this.chain}`
         )
 
         this.processData(response)
