23ac80f65f836443290d2e59c7ffbab290789fc3
romanornr <romanornr@gmail.com>
2023-07-06 15:00:12 +0200

    Status API

---
+++ webserver/status.go
@@ -0,0 +1,23 @@
+package webserver
+
+import (
+	"github.com/go-chi/render"
+	"net/http"
+	"time"
+)
+
+type Status struct {
+	Status string `json:"status"`
+	Uptime string `json:"uptime"`
+}
+
+func handleStatus(w http.ResponseWriter, r *http.Request) {
+	uptime := time.Since(startTime)
+	status := Status{
+		Status: "running",
+		Uptime: uptime.String(),
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	render.JSON(w, r, status)
+}
